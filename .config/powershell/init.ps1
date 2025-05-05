3fcf68fad08390a17655d053c78c511013b1dccb
Possawat Chandruaeng <possawat_163@hotmail.com>
2025-05-05 16:33:53 +0700

    update/fix script and config

--- .config/powershell/init.ps1
+++ .config/powershell/init.ps1
@@ -1,1 +1,16 @@
-New-Item -ItemType SymbolicLink -Value "$(Get-Location)" -Path "$(Split-Path $PROFILE.CurrentUserCurrentHost)"
+#Requires -RunAsAdministrator
+$ConfigPath = "$(Get-Location)"
+Write-Debug $ConfigPath
+
+if ($ConfigPath.CompareTo("*crunchy-dotfile\.config\powershell") -ne 1){
+    Write-Output "Please change the working directory to the script location"
+    Exit
+}
+
+$PowershellProfilePath = $PROFILE.CurrentUserCurrentHost
+if (Test-Path $PowershellProfilePath) {
+    Write-Output "Please delete/back-up existing powershell profile"
+    Exit
+}
+$PowershellDirectoryPath = $(Split-Path -Path $PowershellProfilePath)
+New-Item -ItemType SymbolicLink -Value $ConfigPath -Path $PowershellDirectoryPath
