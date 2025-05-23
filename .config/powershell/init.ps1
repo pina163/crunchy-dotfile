1dc83e8925ee79c7527ed1a0694ce4dd8a23ad5d
Possawat Chandruaeng <possawat_163@hotmail.com>
2025-05-23 22:55:39 +0700

    write-output to write-host and hide New-Item output

--- .config/powershell/init.ps1
+++ .config/powershell/init.ps1
@@ -2,18 +2,18 @@ $ConfigPath = "$(Get-Location)"
 Write-Debug $ConfigPath
 
 if ($ConfigPath.CompareTo("*crunchy-dotfile\.config\powershell") -ne 1){
-    Write-Output "Please change the working directory to the script location"
+    Write-Host "Please change the working directory to the script location"
     Exit
 }
 
 $PowershellProfilePath = $PROFILE.CurrentUserCurrentHost
 if (Test-Path $PowershellProfilePath) {
-    Write-Output "Please delete/back-up existing powershell profile"
+    Write-Host "Please delete/back-up existing powershell profile"
     Exit
 }
 $PowershellDirectoryPath = $(Split-Path -Path $PowershellProfilePath)
 # Use Junction substitute of `-ItemType SymbolicLink`
-New-Item -ItemType Junction -Value $ConfigPath -Path $PowershellDirectoryPath
+$null = New-Item -ItemType Junction -Value $ConfigPath -Path $PowershellDirectoryPath
 
-Write-Output "Reload Profile.."
+Write-Host "Reload Profile.." -NoNewline
 . $PowershellProfilePath
