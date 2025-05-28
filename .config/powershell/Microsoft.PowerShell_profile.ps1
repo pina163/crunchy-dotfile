49ac31f17e995717b87a30e1dde754697b18a251
Possawat Chandruaeng <possawat_163@hotmail.com>
2025-05-28 10:58:08 +0700

    remove old powershell config

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-$Env:GIT_SSH = $((Get-Command -Name ssh).Source)
-if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-    oh-my-posh init pwsh --config "$HOME/crunchy-dotfile/.config/ohmyposh/lean.omp.toml" | Invoke-Expression
-}
-if (Get-Command zoxide -ErrorAction SilentlyContinue) {
-    Invoke-Expression (& { (zoxide init powershell | Out-String) })
-    Set-Alias -Name cd -Value z -Option AllScope -Scope Global -Force
-}
