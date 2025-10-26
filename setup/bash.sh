2fc2490d45683a10a3a48d684b690bbb7fd30d05
possawat.c <possawat_163@hotmail.com>
2025-10-26 23:12:34 +0700

    add neovim setup, update linux setup

--- setup/bash.sh
+++ setup/bash.sh
@@ -1,26 +1,50 @@
 # /usr/bin/env bash
-if ! command -v oh-my-posh &>/dev/null
-then
-	if ! command -v zip &>/dev/null
-	then
-		sudo apt-get install zip
-	fi
+
+# prep utilitie command
+if ! command -v zip &>/dev/null; then
+	echo "install zip"
+	sudo apt-get install zip
+fi
+if ! command -v curl &>/dev/null; then
+	echo "install curl"
+	sudo apt-get install curl
+fi
+if ! command -v stow &>/dev/null; then
+	echo "install stow"
+	sudo apt-get install stow
+fi
+
+# install tools
+if ! command -v oh-my-posh &>/dev/null; then
+	echo "install oh-my-posh"
 	curl -s https://ohmyposh.dev/install.sh | bash -s
 fi
-if ! command -v zoxide &>/dev/null;
-then
+if ! command -v zoxide &>/dev/null; then
+	echo "install fzf and zoxide"
 	sudo apt-get install fzf zoxide
 fi
 
-# TODO stow with dotfiles dir location
-SCRIPT_DIR=$( cd -P -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+if ! command -v nvim &>/dev/null; then
+	echo "install nvim"
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+	sudo rm -rf /opt/nvim-linux-x86_64
+	sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+fi
+
+# setup stow
+SCRIPT_DIR=$( cd -P -- "$( dirname -- "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )
 CRUNCHY_DIR=$(dirname -- $SCRIPT_DIR)
 DOTFILE_DIR="$CRUNCHY_DIR/dotfiles"
-if ! command -v stow &>/dev/null
-then
+
+if ! command -v stow &>/dev/null; then
 	echo "GNU/stow is not available not automatically link dotfiles and source"
 	exit 0
 fi
 
-stow -d $DOTFILE_DIR -t $HOME .
+echo "start restow the dotfiles"
+stow -R -d $DOTFILE_DIR -t $HOME --dotfiles .
+echo "complete restow the dotfiles"
+
+echo "source .bashrc"
 source $HOME/.bashrc
+echo "complete..."
